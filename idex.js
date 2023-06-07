f6693447c12c5cc028b8f78d71545c3dc129d542
AchinthaPrabash3 <achinthaprabash3@gmail.com>
2023-06-07 16:31:09 +0530

    adding js

---
+++ idex.js
@@ -0,0 +1,13 @@
+const observer = new IntersectionObserver(entries => {
+    // Loop over the entries
+    entries.forEach(entry => {
+      // If the element is visible
+      if (entry.isIntersecting) {
+        // Add the animation class
+        entry.target.classList.add('animation');
+      }
+    });
+  });
+  
+  observer.observe(document.querySelector('.text_04'));
+  observer.observe(document.querySelector('.text_05'));
