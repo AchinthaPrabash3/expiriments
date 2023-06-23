ced0f2f9572dbdec5831a29f35df89f00bb54314
AchinthaPrabash3 <achinthaprabash3@gmail.com>
2023-06-23 09:37:31 +0530

    formated

--- idex.js
+++ idex.js
@@ -1,12 +1,12 @@
-const observer = new IntersectionObserver(entries => {
-    // Loop over the entries
-    entries.forEach(entry => {
-      // If the element is visible
-      if (entry.isIntersecting) {
-        // Add the animation class
-        entry.target.classList.add('animation');
-      }
-    });
+const observer = new IntersectionObserver((entries) => {
+  // Loop over the entries
+  entries.forEach((entry) => {
+    // If the element is visible
+    if (entry.isIntersecting) {
+      // Add the animation class
+      entry.target.classList.add("animation");
+    }
   });
-  
-  observer.observe(document.querySelector('.text_04'));
+});
+
+observer.observe(document.querySelector(".text_04"));
